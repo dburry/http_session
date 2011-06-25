ab9da69aefa9e97581a45e6210d4052c42304a9e
David Burry <dburry@falcon>
2011-06-24 23:55:28 -0700

    added lots more tests

--- test/test_http_session.rb
+++ test/test_http_session.rb
@@ -18,6 +18,26 @@ class TestHttpSession < Test::Unit::TestCase
       should('ssl port') { assert_equal 444,  @c.port_or_default(444,  true)  }
     end
     
+    context 'get' do
+      setup { @i = @c.use('foo') }
+      should('work')     { assert_equal @i,  @c.get('foo') }
+      should('fail')     { assert_equal nil, @c.get('bar') }
+      should('nossl')    { assert_equal @i,  @c.get('foo', false) }
+      should('bad ssl')  { assert_equal nil, @c.get('foo', true) }
+      should('port')     { assert_equal @i,  @c.get('foo', false, 80) }
+      should('bad port') { assert_equal nil, @c.get('foo', false, 8080) }
+    end
+    
+    context 'exists?' do
+      setup { @i = @c.use('foo') }
+      should('work')     { assert_equal true,  @c.exists?('foo') }
+      should('fail')     { assert_equal false, @c.exists?('bar') }
+      should('nossl')    { assert_equal true,  @c.exists?('foo', false) }
+      should('bad ssl')  { assert_equal false, @c.exists?('foo', true) }
+      should('port')     { assert_equal true,  @c.exists?('foo', false, 80) }
+      should('bad port') { assert_equal false, @c.exists?('foo', false, 8080) }
+    end
+    
   end
   context 'simple instance methods' do
     setup { @c = HttpSession.new('foo', false, nil) }
@@ -46,6 +66,18 @@ class TestHttpSession < Test::Unit::TestCase
     
   end
   
+  context 'bad request type' do
+    should 'fail with message' do
+      begin
+        HttpSession.use('localhost', false, TEST_SERVER_PORT).request('/', {}, :foo)
+      rescue ArgumentError => e
+        assert_equal('bad type: foo', e.message)
+      else
+        fail 'nothing was raised'
+      end
+    end
+  end
+  
   context 'server' do
     teardown { stop_server }
     
@@ -55,7 +87,48 @@ class TestHttpSession < Test::Unit::TestCase
       should('use/request') { assert_equal 'pong', HttpSession.use('localhost', false, TEST_SERVER_PORT).request('/ping').body }
       should('post_request_url') { assert_equal 'pong', HttpSession.post_request_url("http://localhost:#{TEST_SERVER_PORT}/ping", {'foo' => 'bar'}).body }
       should('use/request post') { assert_equal 'pong', HttpSession.use('localhost', false, TEST_SERVER_PORT).request('/ping', {}, :post, {'foo' => 'bar'}).body }
+      
+      should 'close' do
+        assert_equal 'pong', HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/ping").body
+        sesn = HttpSession.get('localhost', false, TEST_SERVER_PORT)
+        assert_not_nil sesn
+        assert_equal true, sesn.handle.started?
+        sesn.close
+        assert_equal false, sesn.handle.started?
+      end
+      should 'delete' do
+        assert_equal 'pong', HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/ping").body
+        sesn = HttpSession.get('localhost', false, TEST_SERVER_PORT)
+        assert_not_nil sesn
+        sesn.close
+        sesn.delete
+        sesn = HttpSession.get('localhost', false, TEST_SERVER_PORT)
+        assert_equal nil, sesn
+      end
     end
+    
+    context 'redirect' do
+      setup do
+        start_server do |server|
+          server.mount_proc('/ping', Proc.new { |req, resp| resp.body = 'pong' })
+          server.mount_proc('/redir', Proc.new { |req, resp| resp.set_redirect(::WEBrick::HTTPStatus::Found, "http://localhost:#{TEST_SERVER_PORT}/ping") })
+        end
+      end
+      should('work') { assert_equal 'pong', HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/redir").body }
+    end
+    
+    # context 'redirect without host' do
+    #   setup do
+    #     start_server do |server|
+    #       server.mount_proc('/ping', Proc.new { |req, resp| resp.body = 'pong' })
+    #       # note: webrick is smart and converts this to a properly-formed redirect with http://host:port
+    #       # and it does it if I set the headers directly too... so there doesn't seem to be any way to
+    #       # exercise our bad-webserver-redirect-handling code using webrick...
+    #       server.mount_proc('/redir', Proc.new { |req, resp| resp.set_redirect(::WEBrick::HTTPStatus::Found, "/ping") })
+    #     end
+    #   end
+    #   should('work') { assert_equal 'pong', HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/redir").body }
+    # end
     
     context 'redirect 10x' do
       setup do
@@ -101,5 +174,43 @@ class TestHttpSession < Test::Unit::TestCase
       end
     end
     
+    context 'sent cookie' do
+      setup do
+        start_server do |server|
+          server.mount_proc('/ping', Proc.new { |req, resp|
+            resp.cookies << ::WEBrick::Cookie.new('foo', 'bar')
+            resp.body = 'pong'
+          })
+        end
+      end
+      should 'be received' do
+        assert_equal 'pong', HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/ping").body
+        assert_equal 'bar',  HttpSession.get('localhost', false, TEST_SERVER_PORT).cookies['foo']
+      end
+    end
+    
+    context 'sent/expected cookie' do
+      setup do
+        start_server do |server|
+          # set a cookie when you go to /get
+          server.mount_proc('/get', Proc.new { |req, resp|
+            resp.cookies << ::WEBrick::Cookie.new('foo', 'bar')
+            resp.body = 'hereyago'
+          })
+          # check if the cookie was sent back when you go to /check, and return 'set' or 'BAD' in the body indicating
+          server.mount_proc('/check', Proc.new { |req, resp|
+            resp.body = req.cookies[0].name == 'foo' && req.cookies[0].value == 'bar' ? 'set' : 'BAD'
+          })
+        end
+      end
+      should 'be received and sent back' do
+        assert_equal 'hereyago', HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/get").body
+        assert_equal 'set',      HttpSession.get_request_url("http://localhost:#{TEST_SERVER_PORT}/check").body
+      end
+    end
+    
+    # TODO: test SSL!
+    # TODO: test retry_limit
+    # TODO: test keepalives!
   end
 end
